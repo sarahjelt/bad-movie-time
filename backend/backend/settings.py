1ec97cccd20ffd3e852529bab7faed7de6b227ab
Sara Hjelt <s.reb.thom@gmail.com>
2019-07-09 09:25:42 -0400

    Add missing whitenoise app to settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
     'badmovietime',
+    'whitenoise.runserver_nostatic',
 ]
 
 MIDDLEWARE = [
@@ -81,13 +82,14 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'bad_movie_time',
-        'USER': 'sarahjelt',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '5432',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': 'bad_movie_time',
+        # 'USER': 'sarahjelt',
+        # 'PASSWORD': 'password',
+        # 'HOST': 'localhost',
+        # 'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
