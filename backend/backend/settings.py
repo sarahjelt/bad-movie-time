fd8c242306e2a80bf7128311c1954ade8b060cd3
Sara Hjelt <s.reb.thom@gmail.com>
2019-07-09 09:40:37 -0400

    Change settings back to postgres

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -82,14 +82,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME': 'bad_movie_time',
-        # 'USER': 'sarahjelt',
-        # 'PASSWORD': 'password',
-        # 'HOST': 'localhost',
-        # 'PORT': '5432',
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bad_movie_time',
+        'USER': 'sarahjelt',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
