4f277305fb54faba03b481264569d72c64cb1f91
Sara Hjelt <s.reb.thom@gmail.com>
2019-07-09 22:23:51 -0400

    change location of staticfiles

--- backend/settings.py
+++ backend/settings.py
@@ -145,7 +145,7 @@ STATICFILES_DIRS = [os.path.join(REACT_APP_DIR, 'build', 'static')]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, '..', 'staticfiles')
 
 STATIC_URL = '/static/'
 
